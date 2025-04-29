44d5d9840cef9518b5b90e187ee8570130a25956
Austin Drummond <adrum0x7@gmail.com>
2025-04-29 00:47:24 -0400

    added static serving

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import fs from 'fs/promises'
+import mime from 'mime-types'
+import { serveStatic } from "hono/serve-static";
 
 const app = new Hono()
+
+// Serve static files
+app.use('/*', serveStatic({
+  root: './public',
+  getContent: async (path, c) => {
+    try {
+      const data = await fs.readFile(path);
+      let contentType = mime.contentType(path) ?? "text/plain";
+
+      return new Response(data, {
+        headers: {
+          "Content-Type": contentType.toString(),
+        },
+      });
+    } catch (error) {
+      return null;
+    }
+  },
+}))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
